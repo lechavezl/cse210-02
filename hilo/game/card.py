620690c1d00204550866747b2addc5748aac746b
lechavezl <luisernestochavezleal@gmail.com>
2022-11-16 00:33:13 -0400

    Modified classes and methods

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,38 +1,32 @@
 import random
-from game.dealer import Dealer
 
 class Card:
 
     def __init__(self):
 
-        card_number = 0
-        next_card = 0
-        shown = True
-        dealer = Dealer()
-        points = 0
-    
-    def show_current_card(self):
-
-        # RECORDAR tengo que comparar si la respuesta del jugador (h/l) es mayor o menor que el card_number
-        # Respectuvamente, si es as[i, asignar el valor adecuado]
-        print(f"Your card is: {self.card_number}")
-
-    
-    def compare_card_number(self):
-        
-        if dealer.player_guess.lower() == "h" and self.card_number > self.next_card:
-            self.points = 100
-        
-        elif dealer.player_guess.lower() == "l" and self.card_number < self.next_card:
-            self.points = 100
-        
-        else:
-            points = 0
-
-        # if dealer.player_guess == "h" and # y luego compararlo con la siguiente carta si la carta no es igual a la anterior
+        self.card_number = 0
+        self.next_card_number = 0
     
     def draw_card(self):
         
         self.card_number = random.randint(1, 13)
+        return self.card_number
+    
+    def draw_next_card(self):
 
-        self.next_card = random.randint(1, 13)
+        self.next_card_number = random.randint(1, 13)
+        return self.next_card_number
+
+    def compare_card_number(self, player, c_card, n_card, points):
+        
+        if (player.lower() == "h") and (c_card < n_card):
+            points += 100
+            print(f"Your score is: {points}")
+        
+        elif (player.lower() == "l") and (c_card > n_card):
+            points += 100
+            print(f"Your score is: {points}")
+        
+        else:
+            points -= 75
+            print(f"Your score is: {points}")
--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -6,62 +6,99 @@ class Dealer:
     #The responsibility of th Dealer is to control the game and calculate the points
     #earned by the player
 
-    #Attributes =>
-    #dealer: A list of card instances
-    #points: The player's points at the start of the game
-    #total_points: The total player's points at the end of the game
-    #is_playing: A boolean that makes the game running until the players ends thhe game.
-    #guess: 
+    # Attributes =>
+    # dealer: A list of card instances
+    # points: The player's points at the start of the game
+    # total_points: The total player's points at the end of the game
+    # is_playing: A boolean that makes the game running until the players ends thhe game.
+    # guess: 
 
     def __init__(self):
-        #The constructor of the Dealer
+        # The constructor of the Dealer
 
-        #Args: self (Dealer): an instance of Dealer
+        # Args: self (Dealer): an instance of Dealer
 
-        card_instances = []
-        points = 300
-        total_points = 0
-        is_playing = True
-        player_guess = input("")
-        card = Card()
+        self.card_instances = []
+        self.points = 300
+        self.total_points = 0
+        self.is_playing = True
+        self.card = Card()
+        self.player_guess = ""
     
     def start_game(self):
 
-
         while self.is_playing:
             self.guess_card()
-            self.update_score()
-            self.output_score()
+            # self.calculate_score()
+            # self.output_score()
     
     def guess_card(self):
-        #Ask tue user to guess if the next card
-        #is higher or lower
+        # Show card
+        # Ask the user to guess if the next card is higher or lower
+        # calculate the show the score
 
         #Args: self (Dealer): an instance of Dealer
+        current_card = self.card.draw_card()
+        next_card = self.card.draw_next_card()
+        
+        print(f"The card is: {current_card}")
+        self.player_guess = input("Higher or lower? [h/l] ")
 
-        player_guess = input("Higher or lower? [h/l]")
-        self.is_playing = (player_guess == "y")
+        if current_card != next_card:
+            print(f"Next card was: {next_card}")
+
+        if (self.player_guess.lower() == "h") and (current_card < next_card):
+            self.points += 100
+            print(f"Your score is: {self.points}")
+        
+        elif (self.player_guess.lower() == "l") and (current_card > next_card):
+            self.points += 100
+            print(f"Your score is: {self.points}")
+        
+        else:
+            self.points -= 75
+            print(f"Your score is: {self.points}")
+        
+        self.player_guess = input("Play again? [y/n] ")
+        self.is_playing = (self.player_guess == "y")
+        print()
     
-    def update_score(self):
-        #This method calculates and updates player's score
-        
-        #Args: self (Dealer): an instance of Dealer
+    def calculate_points(self, guess, c_card, n_card, score):
 
-        if not self.is_playing:
-            return
+        # This method calculates and updates player's score
         
-        for i in range(len(self.card_instances)):
-            card = self.card_instances[i]
-            card.show_current_card()
-            self.points += card.points
-        self.total_points += self.points
+        # Args: self (Dealer): an instance of Dealer
+
+            if (guess.lower() == "h") and (c_card < n_card):
+                score += 100
+                print(f"Your score is: {score}")
+            
+            elif (guess.lower() == "l") and (c_card > n_card):
+                score += 100
+                print(f"Your score is: {score}")
+            
+            else:
+                score -= 75
+                print(f"Your score is: {score}")
+            
+            self.total_points += self.points
+            
+            return self.total_points
+
+        # self.card.compare_card_number()
+        # self.points += self.card.score
+
+        # for i in range(len(self.card_instances)):
+        #     card = self.card_instances[i]
+        #     self.points += card.points
+        # self.total_points += self.points
 
     def output_score(self):
-        #This method shows the score, current card, next card, and ask the players if they want to play again.
+        # This method shows the score, current card, next card, and ask the players if they want to play again.
 
-        #Args: self (Dealer): an instance of Dealer
+        # Args: self (Dealer): an instance of Dealer
 
         if not self.is_playing:
             return
-        value
-        print(f"The card is: {generate_card}")
+        # value
+        # print(f"The card is: {generate_card}")
